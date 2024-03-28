ad63c9f7133bebc5c037e00210cdbeb4b4706ee6
Malte I.Lauterbach <malte@enigma-blog.de>
2024-03-28 11:33:32 +0100

    trying to make pins clickable

--- components/BlinkingDottedMapComponent.js
+++ components/BlinkingDottedMapComponent.js
@@ -1,26 +1,37 @@
-// components/BlinkingDottedMapComponent.js
 import React, { useEffect, useRef } from 'react';
 
-const BlinkingDottedMapComponent = ({ svgMap }) => {
+const DottedMapComponent = ({ svgMap }) => {
   const mapRef = useRef();
 
   useEffect(() => {
+    const handlePinClick = (event) => {
+      const title = event.target.getAttribute('title');
+      console.log('Pin title:', title);
+      // You can perform any other action here, such as displaying the title in a popup
+    };
+
     if (mapRef.current) {
       const pins = mapRef.current.querySelectorAll('.pin');
-      pins.forEach((pin) => {
-        pin.style.animation = 'blink 15.5s infinite';
+      pins.forEach(pin => {
+        pin.addEventListener('click', handlePinClick);
       });
+
+      // Clean up event listeners when component unmounts
+      return () => {
+        pins.forEach(pin => {
+          pin.removeEventListener('click', handlePinClick);
+        });
+      };
     }
   }, [svgMap]);
 
   return (
     <div
       ref={mapRef}
-      className="dotted-map"
+      className="map-container"
       dangerouslySetInnerHTML={{ __html: svgMap }}
-      style={{ width: '100%', height: 'auto' }}
     />
   );
 };
 
-export default BlinkingDottedMapComponent;
+export default DottedMapComponent;
--- components/DottedMapComponent.js
+++ components/DottedMapComponent.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
+// It takes two props: events and svgMap
 const DottedMapComponent = ({ events, svgMap }) => {
+  // Render a container div with class name 'map-container'
+  // Inside the container, render an img element
+  // The img src is set to a data URI containing the SVG map, which is passed as props
   return (
     <div className="map-container">
       <img src={`data:image/svg+xml;utf8,${encodeURIComponent(svgMap)}`} alt="Dotted Map" />
@@ -9,4 +13,3 @@ const DottedMapComponent = ({ events, svgMap }) => {
 };
 
 export default DottedMapComponent;
-
